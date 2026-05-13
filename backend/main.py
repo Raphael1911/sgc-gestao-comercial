ce9e4053678b212e74963fcb946d0b8047d0379c
Raphael Trindade <raphaeltrindadeolho@gmail.com>
2026-05-13 19:49:05 -0300

    feat(security): corrige tipagem do slowapi e adiciona workspace do vscode

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
 import models, schemas
 from database import engine, get_db
 
-# Cria as tabelas no MySQL automaticamente se não existirem
-# models.Base.metadata.create_all(bind=engine)
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(title="SGC API", version="1.0.0")
 
-@app.get("/")
-def health_check():
-    return {"status": "ok", "mensagem": "Backend SGC operante!"}
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) # type: ignore
 
-# Exemplo de rota já conectada no banco para sua equipe seguir de base
+origens_permitidas = [
+    "http://localhost:5173", 
+   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origens_permitidas,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+@app.get("/")
+@limiter.limit("5/minute")
+def health_check(request: Request): 
+    return {"status": "ok", "mensagem": "Backend SGC operante e blindado!"}
+
 @app.get("/produtos", response_model=list[schemas.ProdutoResponse])
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = db.query(models.Produto).all()
